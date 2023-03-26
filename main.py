ca9c608bf541330c34bd6561d99ee5dc24535afa
Wamolve <66585554+Wamolve@users.noreply.github.com>
2023-03-26 11:24:42 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,27 @@
 from flask import Flask, request
 import logging
 import json
-
 app = Flask(__name__)
-
 logging.basicConfig(level=logging.DEBUG)
-
 @app.route("/", methods=["POST"])
 def main():
     logging.info(request.json)
 
     response = {
-        "text": request.json["version"],
-        "text": request.json["session"],
+        "version": request.json["version"],
+        "session": request.json["session"],
         "response": {
             "end_session": False
         }
-        "text": "Привет! Меня зовут алиса. Я могу помочь с сочинением историй и др."
+        "text": "Привет!"
     }
-    
-    handle_dialog(response, request.json)
-    return json.dumps(response)
 
-
-def handle_dialog(res,req):
-    if req['request']['original_utterance']:
-        ## Проверяем, есть ли содержимое
-        res['response']['text'] = req['request']['original_utterance']
+    req = request.json
+    if req["session"]["new"]:
+        response["response"]["text"] = "Привет! Как твои дела? Как отметил новый год?"
     else:
-        ## Если это первое сообщение — представляемся
-        res['response']['text'] = "Я echo-bot, повторяю за тобой"
-    
-if __name__ == '__main__':
-    app.run()
+        if req["request"]["original_utterance"].lower() in ["хорошо", "отлично"]:
+            response["response"]["text"] = "Супер! Я за вас рада!"
+        elif req["request"]["original_utterance"].lower() in ["плохо", "скучно"]:
+            response["response"]["text"] = "Это печально... нужно было позвать меня!"
+    return json.dumps(response)
