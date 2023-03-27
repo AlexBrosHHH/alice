6dad174ba02b306d695e153fc82bf2f7307db7b0
Wamolve <66585554+Wamolve@users.noreply.github.com>
2023-03-27 16:07:08 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
-from alice_scripts import Skill, request, say, suggest
-#import logging
-#import json
+import logging
+import json
 import requests
 
 app = Flask(__name__)
@@ -9,6 +8,8 @@ app = Flask(__name__)
 #logging.basicConfig(level=logging.DEBUG)
 
 url = "https://api.writesonic.com/v2/business/content/chatsonic?engine=premium"
+
+poluch = False
 
 payload = {
     "enable_google_results": False,
@@ -25,24 +26,27 @@ headers = {
 def main():
     data = request.json
     command = data.get('request', {}).get('command', '')
+    response_text = ''
     end_session = False
 
     if "начнём" in command:
         response_text = "Какую тему ты хочешь выбрать для сочинения?"
-    elif "описание" in command:
-        response_text = "Какое краткое описание твоего сочинения?"
     elif "сочинение" in command:
         # отправляем запрос к API нейросети
-        yield say(<speaker audio="dialogs-upload/439d35b5-5924-4db3-87e3-2ab406bb8683/4c86841e-5318-4c2f-99b7-8b44f6e0596f.opus">)
+        response_text = "Играем музыку!"
+        response = {
+            'response': {
+                'tts': "<speaker audio='dialogs-upload/439d35b5-5924-4db3-87e3-2ab406bb8683/4c86841e-5318-4c2f-99b7-8b44f6e0596f.opus'>"
+            }
+        }
+        print("Отправил")
         response = requests.post(url, json=payload, headers=headers)
         # обрабатываем ответ от нейросети
         if response.status_code == 200:
-            response_text = "Всё готово."
+            print("получил")
             response_text = response.json().get('data', {}).get('text', '')
-            print("Получил")
         else:
             response_text = "Что-то пошло не так при генерации сочинения."
-            print(response.status_code)
         end_session = True
 
     response = {
@@ -52,4 +56,4 @@ def main():
         },
         'version': '1.0'
     }
-    return response
+    return jsonify(response)
