4511ca021af9cf9951cdfd3c3b952ee81e934129
Wamolve <66585554+Wamolve@users.noreply.github.com>
2023-03-28 11:48:06 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,110 +1,75 @@
-почему в яндекс диалогах выдаёт ошибку "1. недопустимый ответ "
-
-import json
-import logging
-import requests
-#from random import randint
-
-# Импортируем подмодули Flask для запуска веб-сервиса.
 from flask import Flask, request
+import openai
 
 app = Flask(__name__)
-url = "https://api.writesonic.com/v2/business/content/ai-article-writer-v2?engine=average&language=ru&num_copies=1"
-logging.basicConfig(level=logging.DEBUG)
 
-# Хранилище данных о сессиях.
-sessionStorage = {}
+# Здесь необходимо указать свой API-ключ для OpenAI
+openai.api_key = "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"
 
-headers = {
-                "accept": "application/json",
-                "content-type": "application/json",
-                "X-API-KEY": "315f79b7-8037-4e1c-a35d-bf255c6bfd24"
-            }
-# Задаем параметры приложения Flask.
-@app.route("/", methods=['POST'])
-def main():
-    logging.info('Request: %r', request.json)
+# Это начальный текст, который Алиса отправляет пользователю
+START_MESSAGE = "Привет! Я генератор сочинений. Я могу сгенерировать любое сочинение по теме, либо по краткому описанию! Начнем?"
 
-    response = {
-        "version": request.json['version'],
-        "session": request.json['session'],
-        "response": {
-            "end_session": False
-        }
-    }
-    handle_dialog(request.json, response)
-
-    logging.info('Response: %r', response)
-    return json.dumps(
-        response,
-        ensure_ascii=False,
-        indent=2
-    )
+# Это сообщение, которое отправляется пользователю, если не удалось получить ответ от OpenAI
+ERROR_MESSAGE = "Извините, произошла ошибка. Попробуйте еще раз."
 
 
-def handle_dialog(req, res):
-    user_id = req['session']['application']['application_id']
-    if req['session']['new']:
-        sessionStorage[user_id] = {
-            'suggests': [
-                "спорт",
-                "технологии",
-                "наука",
-                "здоровье",
-                "природа",
-                "начнём",
-            ]
-        }
-    if req['session']['new']:
-        # Это новый пользователь.
-        # Инициализируем сессию и поприветствуем его.
-        res['response'][
-            'text'] = 'Привет! Я генератор сочинений. Я могу сгенерировать любое сочинение по теме, либо по краткому описанию! Начнем?'
-        res['response']['buttons'] = get_suggests(user_id)
-        return
+# Функция, которая генерирует текст на заданную тему с помощью OpenAI
+def generate_essay(topic):
+    # Модель, которая будет использоваться для генерации текста
+    model_engine = "text-davinci-002"
+
+    # Задаем параметры генерации текста
+    prompt = f"Напишите сочинение на тему '{topic}'"
+    temperature = 0.8
+    max_tokens = 2048
+
+    # Генерируем текст с помощью OpenAI API
     try:
-        if req['request']['original_utterance'].lower() in ['помощь', 'что ты умеешь', 'что ты можешь',
-                                                            'что ты умеешь?', 'что ты можешь?', 'кто ты',
-                                                            'зачем ты нужен', 'зачем тебя создали']:
-            res['response'][
-                'text'] = 'Я расскажу тебе свежую новость, нужно только выбрать одну из категорий: спорт, наука, природа, технологии, здоровье' # Вставить текст ПОМОЩЬ
-            res['response']['buttons'] = get_suggests(user_id)
-            return
-        if req['request']['original_utterance'].lower() in ['да', 'хочу', 'конечно', 'ага', 'валяй', 'рассказывай',
-                                                        'слушаю', 'очень', 'весь внимание']:
-            res['response']['text'] = 'Выбери одну из категорий: спорт, наука, природа, технологии, здоровье'
-            res['response']['buttons'] = get_suggests(user_id)
-            return
-        if req['request']['original_utterance'].lower() in [
-            'спорт',
-            'природа',
-            'технологии',
-            'здоровье',
-            'наука',
-        ]:
-            text = req['request']['original_utterance'].lower()
-            print(text)
-            response = requests.post(url, headers=headers)
-    except:
-        if response.status_code == 200:
-            res['response']['text'] = response.json()["data"][0]["output"]
-            res['response']['buttons'] = get_suggests(user_id)
-        else:
-            raise Exception("Ошибка при получении ответа от внешнего сервиса")
-        res['response']['buttons'] = get_suggests(user_id)
+        response = openai.Completion.create(
+            engine=model_engine,
+            prompt=prompt,
+            temperature=temperature,
+            max_tokens=max_tokens,
+        )
+        return response.choices[0].text.strip()
+    except Exception as e:
+        print(f"An error occurred while generating essay: {e}")
+        return None
 
-def get_start_suggest(user_id):
-    session = sessionStorage[user_id]
-    suggests = [
-        {'title': suggest, 'hide': True}
-        for suggest in session["suggests"][5]
-    ]
-    return suggests
-# Функция возвращает две подсказки для ответа.
-def get_suggests(user_id):
-    session = sessionStorage[user_id]
-    suggests = [
-        {'title': suggest, 'hide': True}
-        for suggest in session['suggests'][:4]
-    ]
-    return suggests
+
+# Функция, которая обрабатывает запросы пользователя
+def process_request(user_input):
+    if "сгенерируй сочинение" in user_input:
+        # Если пользователь хочет сгенерировать сочинение, то запускаем процесс генерации
+        topic = request.form["text"]  # Получаем тему из запроса пользователя
+        generated_essay = generate_essay(topic)
+        if generated_essay:
+            return generated_essay
+        else:
+            return ERROR_MESSAGE
+    else:
+        # Если пользователь просто написал текст, отвечаем ему с подсказкой
+        return "Вы можете запросить сочинение, написав 'сгенерируй сочинение'."
+
+
+# Функция, которая возвращает начальное сообщение
+def get_start_message():
+    return START_MESSAGE
+
+
+# Функция, которая обрабатывает POST-запросы от Яндекс.Диалогов
+@app.route("/", methods=["POST"])
+def handle_dialog():
+    # Получаем данные от Яндекс.Диалогов
+    request_data = request.json
+    # Получаем текст, написанный пользователем
+    user_input = request_data["request"]["command"]
+    # Отвечаем пользователю
+    response = {
+        "response": {
+            "text": process_request(user_input),
+            "end_session": False
+        },
+        "version": request_data["version"]
+    }
+    return response
